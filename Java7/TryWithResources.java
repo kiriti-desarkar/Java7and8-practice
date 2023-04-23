61c8212db6562d1ec70c242702b03e54a1fac2c2
kiriti-desarkar <kiriti.desarkar.mails@gmail.com>
2023-04-23 17:46:25 +0530

    Java 7 feature Try with Resources

---
+++ Java7/TryWithResources.java
@@ -0,0 +1,20 @@
+package Java7;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources{
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+    try(BufferedReader br = new BufferedReader(
+        new FileReader(
+            "C:/udemy/java7and8newfeatures/Java7and8-practice/Java7/test.txt"));){
+            String sCurrentLine;
+            while((sCurrentLine = br.readLine()) != null){
+                System.out.println(sCurrentLine);
+            }
+        }
+
+    }
+}
