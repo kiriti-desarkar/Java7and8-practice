abbbd7039c23473f5d779f952b30ddbaa1aa8d5b
kiriti-desarkar <kiriti.desarkar.mails@gmail.com>
2023-04-24 09:48:13 +0530

    urlclassloader

---
+++ Java7/UrlClassLoader/UseUrlClassLoader.java
@@ -0,0 +1,12 @@
+package Java7.UrlClassLoader;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class UseUrlClassLoader{
+  //  URL[] urls = {new URL("file:junit-4.11.jar"),
+  //              new URL("file:commons-io-2.8.0.jar")};
+  //  URLClassLoader loader = new URLClassLoader(urls);
+
+    
+}
